3e8953c67679e1f96c3fd25034bd531688121a78
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-01-01 16:38:27 +0530

    Extension Methods

---
+++ Extension Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very log blog post bla bla bla bla...";
+            var shortenedPost = post.Shorten(5);
+
+           IEnumerable<int> numbers = new List<int>() { 5, 8, 6, 4 };
+           var max= numbers.Max();//extension method real world example
+
+            Console.WriteLine(max);
+
+
+            System.Console.WriteLine(shortenedPost);
+        }
+    }
+}
---
+++ Extension Methods/StringExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System;
+using System.Linq;
+
+namespace System
+{
+    public static class StringExtension
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOF Words must be grater than or equal to 0");
+            if(numberOfWords == 0)
+            {
+                return "";
+            }
+            else
+            {
+                var words = str.Split(' ');
+
+                if(words.Length <= numberOfWords)
+                {
+                    return str;
+                }
+                else
+                {
+                    return string.Join(" ", words.Take(numberOfWords)) + "...";
+                }
+            }
+        }
+    } 
+}
